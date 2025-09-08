6dd2586b5721310419bc3a54e9f74d005c7083f6
DuarteCPedro <pedrohenriqueduartecoelho@gmail.com>
2025-09-08 14:37:25 -0300

    Pequena correção em "Aula002Constantes"

--- AprendendoSintaxeJava/src/edu/java/auladio/tiposevariaveis/Aula002Constantes.java
+++ AprendendoSintaxeJava/src/edu/java/auladio/tiposevariaveis/Aula002Constantes.java
@@ -28,7 +28,7 @@ public class Aula002Constantes {
 		
 		final double VALOR_DE_PI = 3.14;
 		
-		VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
+		//VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
 
 	}
 
